3238182f5ebf70c84a4ae09064402cac7924212f
Tanay Chaturvedi <65492720+Tanay-dev@users.noreply.github.com>
2023-01-21 03:17:19 +0530

    Add files via upload

---
+++ Printing_in_LCD.ino
@@ -0,0 +1,17 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+void setup() 
+{
+  lcd.init();
+  lcd.clear();
+}
+void loop() 
+{
+  lcd.setBacklight(HIGH);
+  lcd.setCursor(3,0);
+  lcd.print("Tari Maano");
+  lcd.setCursor(4,1);
+  lcd.print("LUND");
+}
